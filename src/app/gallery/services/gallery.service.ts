a708432d4b16d3a77d42faa025b74f32afffde01
Advaith Surya Pandravada <padvaithsurya@gmail.com>
2021-07-01 18:28:32 +0530

    Corrected Image imports

--- src/app/gallery/services/gallery.service.ts
+++ src/app/gallery/services/gallery.service.ts
@@ -6,14 +6,14 @@ import { Picsum } from '../models/picsum.model';
 })
 export class GalleryService {
   private picsums: Picsum[] = [
-    {id: '1005', url: 'https://picsum.photos/id/1005/300/300', author: 'Matthew Weibe'},
-    {id: '1006', url: 'https://picsum.photos/id/1006/300/300', author: 'Vladamir Kudinov'},
-    {id: '1011', url: 'https://picsum.photos/id/1011/300/300', author: 'Roberto Nickson'},
-    {id: '1012', url: 'https://picsum.photos/id/1012/300/300', author: 'Scott Webb'},
-    {id: '1015', url: 'https://picsum.photos/id/1015/300/300', author: 'Alexey Topolyanskiy'},
-    {id: '1036', url: 'https://picsum.photos/id/1036/300/300', author: 'Wolfgang Lutz'},
-    {id: '1037', url: 'https://picsum.photos/id/1037/300/300', author: 'Jordan Mcqueen'},
-    {id: '1039', url: 'https://picsum.photos/id/1039/300/300', author: 'Andrew Coelho'},
+    {id: '1005', url: 'assets/images/persona_1.jpg', author: 'Matthew Weibe'},
+    {id: '1006', url: 'assets/images/persona_2.jpg', author: 'Vladamir Kudinov'},
+    {id: '1011', url: 'assets/images/persona_3.jpg', author: 'Roberto Nickson'},
+    {id: '1012', url: 'assets/images/persona_4.jpg', author: 'Scott Webb'},
+    {id: '1015', url: 'assets/images/nature_1.jpg', author: 'Alexey Topolyanskiy'},
+    {id: '1036', url: 'assets/images/nature_2.jpg', author: 'Wolfgang Lutz'},
+    {id: '1037', url: 'assets/images/nature_3.jpg', author: 'Jordan Mcqueen'},
+    {id: '1039', url: 'assets/images/nature_4.jpg', author: 'Andrew Coelho'},
   ];
   constructor() { }
 
